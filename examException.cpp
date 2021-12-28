05407b0cc8620642976536b8fe1ff180cd279579
Torch <70550570+B4night@users.noreply.github.com>
2021-12-28 14:32:18 +0800

    Add files via upload

---
+++ examException.cpp
@@ -0,0 +1,41 @@
+//判断表达式中的括号是否匹配
+#include <iostream>
+#include <stack>
+using namespace std;
+
+bool exam(string s);
+
+int main() {
+    cout << exam("((((([{[]}])))))");
+}
+
+bool exam(string str) {
+    stack<char> s;
+    for (auto& elem : str) {
+        switch (elem) {
+            case '(':
+            case '[':
+            case '{':
+                s.push(elem);
+                break;
+            case ')':
+                if (s.empty() || s.top() != '(')
+                    return false;
+                s.pop();
+                break;
+            case ']':
+                if (s.empty() || s.top() != '[')
+                    return false;
+                s.pop();
+                break;
+            case '}':
+                if (s.empty() || s.top() != '{')
+                    return false;
+                s.pop();
+                break;
+            default:
+                break;
+        }
+    }
+    return s.empty();
+}
